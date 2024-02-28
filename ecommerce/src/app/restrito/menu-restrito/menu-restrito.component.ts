71f35c8898f681628720f456b9c9ae92e4c8bc83
Andrei Silva <andreiluizsilva07@gmail.com>
2024-02-28 11:39:35 -0300

    implementação de componentes

---
+++ ecommerce/src/app/restrito/menu-restrito/menu-restrito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-restrito',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-restrito.component.html',
+  styleUrl: './menu-restrito.component.css'
+})
+export class MenuRestritoComponent {
+
+}
